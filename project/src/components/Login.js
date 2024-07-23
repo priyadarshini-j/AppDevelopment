185560a7975ef3a6d5c3ead4c7f5bd09efb4ee8a
priyadarshini-j <153439458+priyadarshini-j@users.noreply.github.com>
2024-07-23 12:47:54 +0530

    to changes frontend

---
+++ project/src/components/Login.js
@@ -0,0 +1,15 @@
+function Login() {
+    return(
+        <div>
+            <h1>
+                Login
+            </h1>
+            <form>
+                <label>Email:</label>
+                <input type="email" placeholder="Enter email"></input>
+                <button>Login</button>
+            </form>
+        </div>
+    );
+}
+export default Login;
