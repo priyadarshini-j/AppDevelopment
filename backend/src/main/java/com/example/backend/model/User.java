0ffea6db6bef066ad2252afcb03e60066445323d
priyadarshini-j <153439458+priyadarshini-j@users.noreply.github.com>
2024-08-01 11:33:01 +0530

    to create project

---
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -0,0 +1,45 @@
+package com.example.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private int uid;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int uid, String email, String password) {
+        this.uid = uid;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
