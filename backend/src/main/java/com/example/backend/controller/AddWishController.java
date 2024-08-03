e39d881afea45d36f4b651e7d6a6005a8258b139
priyadarshini-j <153439458+priyadarshini-j@users.noreply.github.com>
2024-08-03 20:44:10 +0530

    To create addwish

--- backend/src/main/java/com/example/backend/controller/AddWishController.java
+++ backend/src/main/java/com/example/backend/controller/AddWishController.java
@@ -14,36 +14,37 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.backend.model.CategoryType;
-import com.example.backend.service.CategoryTypeService;
+import com.example.backend.model.AddWish;
+import com.example.backend.service.AddWishService;
+
 
 
 @RestController
-public class CategoryTypeController {
+public class AddWishController {
     @Autowired
-    CategoryTypeService cs;
+    AddWishService as;
     @PostMapping("/post/categorytype")
-    public ResponseEntity<CategoryType>addelements(@RequestBody CategoryType c)
+    public ResponseEntity<AddWish>addelements(@RequestBody AddWish a)
     {
-        CategoryType cst=cs.create(c);
-        return new ResponseEntity<>(cst,HttpStatus.CREATED);
+        AddWish ast=as.create(a);
+        return new ResponseEntity<>(ast,HttpStatus.CREATED);
     }
     @GetMapping("/get/categorytype")
-    public ResponseEntity<List<CategoryType>> showinfo()
+    public ResponseEntity<List<AddWish>> showinfo()
     {
-        return new ResponseEntity<>(cs.getAll(),HttpStatus.OK);
+        return new ResponseEntity<>(as.getAll(),HttpStatus.OK);
     }
     @GetMapping("/get/categorytype/{cid}")
-    public ResponseEntity<CategoryType> getById(@PathVariable Integer cid)
+    public ResponseEntity<AddWish> getById(@PathVariable Integer cid)
     {
-        CategoryType obj=cs.getId(cid);
+        AddWish obj=as.getId(cid);
         return new ResponseEntity<>(obj,HttpStatus.OK);
     }
     @PutMapping("/put/{cid}")
-    public ResponseEntity<CategoryType> putMethodName(@PathVariable("cid") int cid, @RequestBody CategoryType c) {
-        if(cs.update(cid,c) == true)
+    public ResponseEntity<AddWish> putMethodName(@PathVariable("cid") int cid, @RequestBody AddWish a) {
+        if(as.update(cid,a) == true)
         {
-            return new ResponseEntity<>(c,HttpStatus.OK);
+            return new ResponseEntity<>(a,HttpStatus.OK);
         }
         
         return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
@@ -51,7 +52,7 @@ public class CategoryTypeController {
     @DeleteMapping("/delete/{cid}")
     public ResponseEntity<Boolean> delete(@PathVariable("cid") int cid)
     {
-        if(cs.delete(cid) == true)
+        if(as.delete(cid) == true)
         {
             return new ResponseEntity<>(true,HttpStatus.OK);
         }
