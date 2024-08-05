572356a2c8ee27af7aa51e9fb1b99298aa1e01f5
priyadarshini-j <153439458+priyadarshini-j@users.noreply.github.com>
2024-08-05 10:26:55 +0530

    Add files via upload

---
+++ Day7/Product.java
@@ -0,0 +1,113 @@
+package com.example.backend.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Product {
+    @Id
+    private int productId;
+    private String productName;
+    private double productPrice;
+    private String category; 
+
+    public Product() {
+    }
+
+    public Product(int productId, String productName, double productPrice, String category) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.category = category;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_uid")
+    @JsonBackReference
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    //product to buy
+    @OneToOne(mappedBy = "product",cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private Buy buy;
+
+    public Buy getBuy() {
+        return buy;
+    }
+
+    public void setBuy(Buy buy) {
+        this.buy = buy;
+    }
+    //product to feedback
+
+    @OneToOne(mappedBy = "product",cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private Feedback feedback;
+
+    public Feedback getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(Feedback feedback) {
+        this.feedback = feedback;
+    }
+    
+    
+
+    
+
+   
+
+    
+
+}
---
+++ Day7/User.java
@@ -0,0 +1,68 @@
+package com.example.backend.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class User {
+    @Id
+    private int uid;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int uid, String email, String password) {
+        this.uid = uid;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Product> product=new ArrayList<>();
+
+    public List<Product> getOrderdata() {
+        return product;
+    }
+
+    public void setOrderdata(List<Product> product) {
+        this.product= product;
+    }
+
+
+
+    
+    
+}
