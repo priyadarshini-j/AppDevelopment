57cd382d40a8e0b6f57fde79c43546239d67bbfc
priyadarshini-j <153439458+priyadarshini-j@users.noreply.github.com>
2024-08-06 09:38:18 +0530

    Add files via upload

---
+++ Day9/UsersDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.backend.config;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.example.backend.model.User;
+import com.example.backend.repository.UserRepository;
+
+
+
+@Component
+public class UsersDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userInfo = repository.findByEmail(username);
+        return userInfo.map(UsersDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found " + username));
+
+    }
+}
